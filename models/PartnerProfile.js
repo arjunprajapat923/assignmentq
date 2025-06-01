a13d80b1f1c13d86f76789aa2552cc89d3f728fc
arjunprajapat923 <prajapatiarjun68663@gmail.com>
2025-06-01 09:05:57 +0530

    add a new updates

--- models/PartnerProfile.js
+++ models/PartnerProfile.js
@@ -4,7 +4,7 @@ const partnerProfileSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   services: [String],
   documentId: String, // e.g., Aadhar number
-  portfolioSamples: [String], // URLs
+  portfolioSamples: [String], // URLs will contains its images for any other info so  i am not adding extraa image field 
   status: { type: String, enum: ['pending', 'verified', 'rejected'], default: 'pending' },
   adminComment: { type: String },
 }, { timestamps: true });
